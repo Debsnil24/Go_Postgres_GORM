b30ae407b88eea8c3030ad46d1acf251e3d06320
Debsnil24 <debsnilsamudra@gmail.com>
2025-01-06 11:31:39 +0530

    Updation of File Structure **Summary** - Updated the file structure - Created a Book Model - Added the code for Create Book function **To-Do* - Complete the controller function

--- middleware/controller.go
+++ middleware/controller.go
@@ -1,1 +1,31 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/Debsnil24/Go_Postgres_GORM/models"
+	"github.com/Debsnil24/Go_Postgres_GORM/router"
+	"github.com/gofiber/fiber/v2"
+)
+type Repository struct {
+	router.Repository
+}
+
+func(r *Repository) CreateBook(context *fiber.Ctx) error {
+	book := models.Book{}
+	err := context.BodyParser(&book)
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "Request Failed"})
+		return err
+	}
+	err = r.DB.Create(&book).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "Unable to Create Book"})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "Book Created Successfully"})
+		return nil
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,7 @@
 package models
+
+type Book struct {
+	Author		string	`json:"author"`
+	Title		string	`json:"title"`
+	Publisher	string	`json:"publisher"`
+}
