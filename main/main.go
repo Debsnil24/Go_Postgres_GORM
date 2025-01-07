1d9e31ab5224791d6379dc5a08249010f8ce76ef
Debsnil24 <debsnilsamudra@gmail.com>
2025-01-07 17:33:45 +0530

    Created the Postgres Connection Code **Summary** - Created a Function to initialize a New Connection - Created a function to perform Auto Migration on the given model - Added a code to assign the required credentials for the DB connection in the config file **To-Do** - Complete all the remaining controller functions - Complete the .env file

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Debsnil24/Go_Postgres_GORM/models"
 	"github.com/Debsnil24/Go_Postgres_GORM/router"
@@ -17,6 +18,15 @@ func main() {
 		log.Fatal("Couldn't load .env file")
 	}
 
+	config := &storage.Config{
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User: os.Getenv("DB_USER"),
+		SSLMode: os.Getenv("DB_SSL"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+	
 	db, err := storage.NewConnection(config)
 	if err != nil {
 		log.Fatal("Couldn't Load the Database")
--- models/models.go
+++ models/models.go
@@ -18,3 +18,8 @@ type Books struct {
 	Title		*string		`json:"title"`
 	Publisher	*string		`json:"publisher"`
 }
+
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+	return err
+}
